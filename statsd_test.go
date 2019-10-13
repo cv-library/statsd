99f8bf27372f4ade2b6cae35624226d30ecec01a
Mark Raymond <markcharlesraymond@gmail.com>
2019-10-13 10:56:47 +0100

    statsd_test.go: test metric received over UDP

--- statsd_test.go
+++ statsd_test.go
@@ -1,16 +1,41 @@
 package statsd
 
 import (
+	"bytes"
+	"net"
+	"strconv"
 	"testing"
 	"time"
 )
 
 func TestTimerSend(t *testing.T) {
+	conn, err := net.ListenUDP("udp", &net.UDPAddr{Port: 8125})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+
+	conn.SetDeadline(time.Now().Add(time.Second))
+
 	timer := Timer()
 
 	time.Sleep(time.Millisecond)
 
-	if timer.Send("foo").Nanoseconds() == 0 {
-		t.Fail()
+	took := timer.Send("foo")
+
+	if took == 0 {
+		t.Error("Send() took no time")
+	}
+
+	got := make([]byte, 8)
+	if _, _, err := conn.ReadFromUDP(got); err != nil {
+		t.Fatal(err)
+	}
+
+	exp := []byte("foo:" +
+		strconv.FormatUint(uint64(took.Nanoseconds()/1e6), 10) + "|ms")
+
+	if !bytes.Equal(got, exp) {
+		t.Errorf("got: %s; want: %s", got, exp)
 	}
 }
